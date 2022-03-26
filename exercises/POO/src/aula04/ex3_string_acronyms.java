bf53263a4cdb5d81e12194919ed4ae8db132dfd3
Gonçalo <goncalo.lslv.silva@gmail.com>
2022-03-26 14:50:15 +0000

    4th practical script preparation exercises

---
+++ exercises/POO/src/aula04/ex3_string_acronyms.java
@@ -0,0 +1,12 @@
+/** Read a sentence ao show its acronyms. Take into account, that
+ * word with size inferior to 3, don't count.
+ * Example: Universidade de Aveiro -> UA
+ */
+
+package aula04;
+
+public class ex3_string_acronyms {
+    public static void main(String[] args) {
+        
+    }
+}
